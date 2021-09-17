92b77db9a703c490557b79ad0f97e5d974d60583
kwonbosung02 <rnjsqhtjd0526@naver.com>
2021-09-18 01:39:49 +0900

    refactor

--- barcode_generator.py
+++ barcode_generator.py
@@ -2,22 +2,22 @@ from barcode import EAN13
 from barcode.writer import ImageWriter
 import random
 import shutil
-#311 * 80
-#main location, user barcode directory, ecobag barcode directory
+
+# main location, user barcode directory, ecobag barcode directory
 src = './'
 dir_u = './barcode_user/'
 dir_e = './barcode_ecobag/'
 opt ={"font_size": 0, "text_distance": 3, "quiet_zone": 1}
 
 
-#GET Unique Barcode Number for User
+# GET Unique Barcode Number for User
 def generate_number_user(phone) -> str:
     generate_number = phone
     generate_number += str(random.randrange(0,10)) + str(random.randrange(0,10))
 
     return generate_number
 
-#GET Unique Barcode Number for Ecobag
+# GET Unique Barcode Number for Ecobag
 def generate_number_ecobag() -> str:
     number = ''
     for i in range(1,14):
@@ -26,23 +26,23 @@ def generate_number_ecobag() -> str:
         return generate_number_ecobag()
     return number
 
-    #If number already exists in database, then recall this function
+    # If number already exists in database, then recall this function
 
 
 
-#Move User barcode
+# Move User barcode
 def locate_user_barcode(generate_code,type):
     filename = str(generate_code) + type
     shutil.move(src+filename,dir_u+filename)
 
-#Move Ecobag barcode
+# Move Ecobag barcode
 def locate_ecobag_barcode(generate_code,type):
     filename = str(generate_code) + type
     shutil.move(src+filename,dir_e+filename)
 
 
 
-#Generate barcode for User
+# Generate barcode for User
 def generate_barcode_user_png(phone) -> str:
     generate_num = generate_number_user(phone)
     generate_code = EAN13(generate_num, writer=ImageWriter())
@@ -50,7 +50,7 @@ def generate_barcode_user_png(phone) -> str:
     locate_user_barcode(generate_code,'.png')
     return str(generate_code)
 
-#Generate barcode for User
+# Generate barcode for User
 def generate_barcode_user_svg(phone) -> str:
     generate_num = generate_number_user(phone)
     generate_code = EAN13(generate_num)
@@ -60,7 +60,7 @@ def generate_barcode_user_svg(phone) -> str:
 
 
 
-#Generate barcode for Ecobag
+# Generate barcode for Ecobag
 def generate_barcode_ecobag_svg() -> str:
     generate_num = generate_number_ecobag()
     generate_code = EAN13(generate_num)
@@ -68,7 +68,7 @@ def generate_barcode_ecobag_svg() -> str:
     locate_ecobag_barcode(generate_code,'.svg')
     return str(generate_code)
 
-#Generate barcode for Ecobag
+# Generate barcode for Ecobag
 
 def generate_barcode_ecobag_png() -> str:
     generate_num = generate_number_ecobag()
--- bracode_recognition.py
+++ bracode_recognition.py
@@ -1,72 +1,92 @@
+# --------------Import module---------------
 import cv2
 import requests, json
 from pyzbar.pyzbar import decode
 from playsound import playsound
+print('library loaded')
+
+# ----------------linespace-----------------
 def space():
     print('-' * 24)
-print('library loaded')
 space()
+
+# ----------------Market id-----------------
 Market_id_ = 'market-0001'
 print("Market id is "+ Market_id_)
 space()
+
+# ----------------post url------------------
 URL = "http://pwnable.co.kr:8000/market_rent/"
 
-
-
+# --------------local camera----------------
 try:
     cam = cv2.VideoCapture(0)
-    face_cascade = cv2.CascadeClassifier('./data/haarcascade.xml')
 except:
     print('Error')
 
 if __name__ == "__main__":
+    # -------------load cascade-------------
+    face_cascade = cv2.CascadeClassifier('./data/haarcascade.xml')
+
+    # --------------resize cap--------------
     mask = cv2.imread('./cap.png')
     mask_h, mask_w, _ = mask.shape
     resize_h = int(0.3*mask_h)
     resize_w = int(0.3*mask_w)
     resize_mask = cv2.resize(mask,(resize_w,resize_h), interpolation=cv2.INTER_CUBIC)
 
+    # -------------cap bitwise--------------
     mask = cv2.cvtColor(resize_mask, cv2.COLOR_BGR2GRAY)
     mask[mask[:] >= 200] = 0
     mask[mask[:] > 0] = 255
     mask_inv = cv2.bitwise_not(mask)
     cap = cv2.bitwise_and(resize_mask, resize_mask, mask=mask)
+
+    # ----initial barcode and check var-----
     barcode1 = ''
     barcode2 = ''
     check = 0
+
+    # --------------main loop---------------
     while(True):
 
+        # -----------load cam---------------
         ret, frame = cam.read()
 
+        # -----------convert----------------
         gray_scale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-        #face Recognition
+        # --------face Recognition----------
         faces = face_cascade.detectMultiScale(gray_scale, scaleFactor=1.1, minNeighbors=3, minSize=(30, 30))
-        #balance, ftf min pixel, min f size
+        # scaleFactor : balance, minNeighbors : face to face minimum pixel, minsize : minimum face size
 
+        # -----------draw rect--------------
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             try:
                 x_v = -100
                 y_v = -160
+                # ---------masking----------
                 roi = frame[y+y_v:y+resize_h+y_v,x+x_v:x+resize_w+x_v]
-
                 back = cv2.bitwise_and(roi, roi, mask=mask_inv)
-
                 dst = cv2.add(cap, back)
 
                 frame[y+y_v:y+resize_h+y_v,x+x_v:x+resize_w+x_v] = dst
             except:
                 pass
-        #barcode Recognition
+
+        # -------barcode Recognition---------
         barcodes = decode(frame)
 
         if len(barcodes) != 0:
+            # -------update barcode----------
             barcode = barcodes[0]
 
             playsound("./sound/in2.mp3")
             playsound("./sound/in2.mp3")
             check = check + 1
+
+            # -----------check---------------
             if(check == 1):
                 barcode1 = barcode
                 print(barcode1.data.decode('utf-8'))
@@ -75,10 +95,14 @@ if __name__ == "__main__":
                 print(barcode2.data.decode('utf-8'))
             barcodes = []
 
+        # --------------post-----------------
         if(check == 2):
             print("two barcodes are collected")
+            # -------value initialize--------
             user_ = ''
             ecobag_ = ''
+
+            # -------------check-------------
             if barcode1.data.decode('utf-8')[0:3] == '010':
                 user_ = barcode1
                 ecobag_ = barcode2
@@ -86,6 +110,8 @@ if __name__ == "__main__":
                 user_ = barcode2
                 ecobag_ = barcode1
             space()
+
+            # ---------update data-----------
             print("USER : " + user_.data.decode('utf-8')[:-2])
             print("ECOBAG : " + ecobag_.data.decode('utf-8'))
             print("MARKET : " + Market_id_)
@@ -94,6 +120,7 @@ if __name__ == "__main__":
                     "Eid"  : str(ecobag_.data.decode('utf-8'))
                     }
 
+            # -----------req post------------
             try:
                 res = requests.post(URL,data=json.dumps(data))
                 space()
@@ -103,10 +130,10 @@ if __name__ == "__main__":
                 print('POST ERROR')
             check = 0
             barcodes = []
-
+        # ------------show video-------------
         cv2.imshow('main camera', frame)
 
-
+        # ----------destroy key--------------
         end_out_key = cv2.waitKey(10) & 0xFF
         if end_out_key == 27:
             break
