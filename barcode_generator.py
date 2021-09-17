6263648045a1c83bccd31a80b3d4bd7c2c89c980
kwonbosung02 <rnjsqhtjd0526@naver.com>
2021-09-17 20:04:13 +0900

    commit

--- barcode_generator.py
+++ barcode_generator.py
@@ -1,8 +1,20 @@
 from barcode import EAN13
 from barcode.writer import ImageWriter
+import random
 
-generate_number = '2021001110023' #year[4] location code[5] count[4]
+def generate_number(phone):
+    generate_number = phone
+    generate_number += str(random.randrange(0,10)) + str(random.randrange(0,10))
+    return generate_number
 
-generated_code = EAN13(generate_number, writer=ImageWriter())
+def generate_barcode_png(phone):
+    generate_num = generate_number(phone)
+    generated_code = EAN13(generate_num, writer=ImageWriter())
+    generated_code.save(generate_num)
+    return generate_num
 
-generated_code.save('code')
+def generate_barcode_svg(phone):
+    generate_num = generate_number(phone)
+    generate_code = EAN13(generate_num)
+    generate_code.save(generate_num)
+    return generate_num
